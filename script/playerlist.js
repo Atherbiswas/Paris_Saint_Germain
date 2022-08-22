263fc858ccad17426ff99f46fbdd6d785596bd75
Ather1230 <ather.biswas0057@gmail.com>
2022-08-22 20:25:37 +0600

    Player list added by js

--- script/playerlist.js
+++ script/playerlist.js
@@ -1,12 +1,28 @@
-document.getElementById('btn-select').addEventListener('click', function(){
-    const playerList = document.getElementById('player-list');
-    const li1 = document.createElement('li');
-    // const messi = document.getElementById('messi'); 
-    li1.innerText = 'Lionel Messi';
-    playerList.appendChild(li1);
-    const li2 = document.createElement('li');
-    li2.innerText = 'Neymar Jr.';
-    playerList.li2.appendChild(li2)
+// declare arry for get player list
+const arryPlayerList = [];
+function display(selectPlayer){
+    const playerAllList = document.getElementById('player-list');
+    playerAllList.innerHTML = '';
+    for(let i = 0; i <arryPlayerList.length; i++){
+       const pName = arryPlayerList[i].playerName;
+       //create tr part...
+       const tr = document.createElement('tr');
+       tr.innerHTML = `
+        <th>${i + 1}.</th>
+        <td>${pName}</td>
+    `;
+    // to add tr into tbody by append child 
+    playerAllList.appendChild(tr);
+    }
+}
+//common function for player select..
+function playerSelect(element){
+    const playerName = element.parentNode.children[0].innerText;
+    const playerObj ={
+        playerName : playerName
+    }
+    arryPlayerList.push(playerObj);
+    document.getElementById('player-number').innerText = arryPlayerList.length;
+    display(arryPlayerList);
     
-
-})
+}
