84d4537f6dc959f89a995571950d053140471caa
Ather1230 <ather.biswas0057@gmail.com>
2022-08-22 14:51:55 +0600

    player list added

---
+++ script/playerlist.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-select').addEventListener('click', function(){
+    const playerList = document.getElementById('player-list');
+    const li1 = document.createElement('li');
+    // const messi = document.getElementById('messi'); 
+    li1.innerText = 'Lionel Messi';
+    playerList.appendChild(li1);
+    const li2 = document.createElement('li');
+    li2.innerText = 'Neymar Jr.';
+    playerList.li2.appendChild(li2)
+    
+
+})
